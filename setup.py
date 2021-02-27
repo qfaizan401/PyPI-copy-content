aeb4d29d730c6eb9ef49158b8832d19aee8528c2
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2021-02-28 02:49:52 +0500

    updated package repo

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="copy-content",
-    version="1.0.2",
+    version="1.0.4",
     author="Muhammad Faizan Qureshi",
     author_email="qfaizan401@gmail.com",
     description="This package is an automated tool for coping multiple files one folder/directory to another with your desired extension.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/qfaizan401/pyPackage-copy-contain",
+    url="https://github.com/qfaizan401/PyPI-copy-content",
     project_urls={
-        "Bug Tracker": "https://github.com/qfaizan401/pyPackage-copy-contain/issues",
+        "Bug Tracker": "https://github.com/qfaizan401/PyPI-copy-content/issues",
     },
     license="MIT",
     classifiers=[
